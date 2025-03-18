b015e431947dca921901e1133c97e8f1a449bc1b
daniel.urbanodelarua <daniel.urbanodelarua@ext.ema.europa.eu>
2025-03-18 12:20:40 +0100

    Refactor package structure and update version to 0.3.4; implement OpenRouter exception handling

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name='openrouter_exception_handler',
-    packages=find_namespace_packages(where='openrouter_exception_handler/', include=['openrouter_exception_handler.exception_handler']),
-    package_dir={'': 'openrouter_exception_handler'},
+    packages=find_packages(where='src'),  # ✅ Ahora encuentra el paquete correctamente
+    package_dir={'': 'src/openrouter_exception_handler'},  # ✅ Mapea el paquete a la carpeta `src`
+    install_requires=['requests'],
 )
